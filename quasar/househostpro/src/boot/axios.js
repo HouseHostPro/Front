bd7cfb44e0518e53930bed5f908205539a3d130a
Cristian <crispmz_3@hotmail.com>
2024-02-09 01:15:48 +0100

    axios

--- quasar/househostpro/src/boot/axios.js
+++ quasar/househostpro/src/boot/axios.js
@@ -1,10 +1,29 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
-export default boot(({ app }) => {
+export default boot(({ app,router }) => {
   app.config.globalProperties.$axios = axios
 
+  axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
 
+  axios.interceptors.request.use(function (config){
+
+    console.log("Status ok",config);
+    config.withCredentials = true;
+    config.headers.set('Authorization',`Bearer ${localStorage.getItem('token')}`) ;
+    return config;
+  },function (rej){
+    console.log("Status ko",rej.response.status);
+  });
+
+  axios.interceptors.response.use(function (response){
+      console.log("Status ok",response.status);
+      return response.data;
+    },function (rej){
+      console.log("Status ko",rej.response.status);
+      router.push("/")
+    }
+  )
 })
 export { axios }
 
